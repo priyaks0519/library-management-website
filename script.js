7217894263ebebc04fa73b74666842f2add5beb8
priyaks0519 <priyaks879@gmail.com>
2026-04-01 12:36:28 +0530

    Push complete Library Management System code

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Library Management System loaded');
+
+function addBook() {
+    // Code to add a book to the library
+}
+
+function removeBook() {
+    // Code to remove a book from the library
+}
